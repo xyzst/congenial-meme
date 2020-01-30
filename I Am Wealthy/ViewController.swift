0e80750e6e977c2112325014be3b6a743a08e0ad
Darren Rambaud <xyzst@users.noreply.github.com>
2020-01-29 19:07:21 -0600

    Define baseline iOS application
    Initial commit for a simple iOS application. Add basic assets, link assets to storyboard
    
    Related to: none

---
+++ I Am Wealthy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Wealthy
+//
+//  Created by Darren Rambaud on 1/28/20.
+//  Copyright © 2020 Darren Rambaud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
